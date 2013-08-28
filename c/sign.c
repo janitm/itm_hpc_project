4710647de72f55fe13ce99fe2654a02f9872e006
Sebastian Arnoldt <sebastian.arnoldt@gmail.com>
2013-08-28 17:19:10 +0200

    updated latex files

---
+++ c/sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+void main()
+{
+    double x = -5.0;
+    printf("sign %f", copysign(1.0, x));
+
+
+}
+
+void function()
+{
+    oisdhg
+
+    if()
+        sugsgh
+        if()
+        {
+            osiuhsg
+        }
+
+
+}
+
+CURLY BRACE
+
