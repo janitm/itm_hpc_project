a0f0fc2d0ba5e31e7ab2bb3a971fe7d432c25ab1
Sebastian Arnoldt <sebastian.arnoldt@gmail.com>
2013-08-28 07:49:57 +0200

    started to write C code
    @ Georgia, we need to think about how close we want to stay to the
    serial structure of the original Fortran code. It is extremely serial.

--- c/functions.c
+++ c/functions.c
@@ -1,3 +1,19 @@
-/*put all the functions here, for instance hadvppm.f */
+#include<stdio.h>
+#include<math.h>
+//#include<openmp.h> //is this the correct compiler directive?
 
+
+//arguments to function hadvppm
+#define HADVPPM_ARGS double* nn, float dt, float dx, double* con, double* vel, double mscl,double* flxarr, double flux1,double flux2,double saflux,double fc1,double fc2
+
+//function prototype
+void hadvppm(HADVPPM_ARGS)
+{
+    printf("hello");
+}
+
+int main()
+{
+    return 0;
+}
 
