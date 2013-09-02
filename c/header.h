ec63627a0cf1b8060d0fec93a432325ce9ca9dc4
Georgia Kanli <kanli.g@gmail.com>
2013-09-02 16:22:31 +0200

    small changes

--- c/header.h
+++ c/header.h
@@ -7,8 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//#include "functions.h"
 //#include <openmp.h> //is this the correct compiler directive?
-#include "functions.c"
 
 //arguments to function hadvppm
 #define HADVPPM_ARGS int nn, double step, double con[], double vel[], double mscl[], double flxarr[], double* flux1, double* flux2, double saflux[], double fc1[], double fc2[]
