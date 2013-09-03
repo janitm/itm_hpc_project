9919f2f553207fb947355ab984a2035197b4f2d5
Sebastian Arnoldt <sebastian.arnoldt@gmail.com>
2013-09-03 15:35:44 +0200

    small changes in wrapper

--- m/wrapper.m
+++ m/wrapper.m
@@ -1,4 +1,4 @@
-nn = 5;
+nn = 90;
 dt = 900;
 dx = 36000;
 conInit = zeros(nn,1) + 1;
@@ -9,4 +9,4 @@ flxarr = zeros(nn,1);
 flux1 = 0;
 flux2 = 0;
 
-[con, flux1, flux2, fluxarr, fm, fp, cm, cl, cr, dc, c6] = hadvppm(nn, dt, dx, conInit, velInit, mscl, flxarr, flux1, flux2);
+hadvppm(nn, dt, dx, conInit, velInit, mscl, flxarr, flux1, flux2);
