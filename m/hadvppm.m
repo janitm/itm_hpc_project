4ef4a5492a8d7d3a2202f88df76e1c6acf9120eb
Sebastian Arnoldt <sebastian.arnoldt@gmail.com>
2013-09-03 14:51:09 +0200

    updated matlab code

--- m/hadvppm.m
+++ m/hadvppm.m
@@ -1,4 +1,4 @@
-function [out] = hadvppm( nn, dt, dx, con, vel, mscl, flxarr, flux1, flux2 )
+function [con, flux1, flux2, flxarr, fm, fp, cm, cl, cr, dc, c6] = hadvppm( nn, dt, dx, con, vel, mscl, flxarr, flux1, flux2 )
 
 %      include "camx.prm"
 %      real con(nn),vel(nn),flxarr(nn),mscl(nn),saflux(nn)
@@ -8,8 +8,11 @@ TWO3RDS = 2./3.;
 
 fm = zeros(nn,1);
 fp = zeros(nn,1);
-cm = zeros(nn,1);
 dc = zeros(nn,1);
+cr = zeros(nn,1);
+cm = zeros(nn,1);
+cl = zeros(nn,1);
+c6 = zeros(nn,1);
 
 %Zero order polynomial at the boundary cells
 
@@ -58,11 +61,11 @@ for i = 2:nn-1
             cr(i) = 3.*con(i) - 2.*cl(i);
         end
     else
-        cl(i) = con(i)
-        cr(i) = con(i)
+        cl(i) = con(i);
+        cr(i) = con(i);
     end
-    dc(i) = cr(i) - cl(i)
-    c6(i) = 6.*(con(i) - 0.5*(cl(i) + cr(i)))
+    dc(i) = cr(i) - cl(i);
+    c6(i) = 6.*(con(i) - 0.5*(cl(i) + cr(i)));
 end
 
 % Compute fluxes from the parabolic distribution
@@ -70,17 +73,15 @@ end
 for i = 2:nn-1
     x = max(0., -vel(i-1)*(dt/dx));
     fm(i) = x*(cl(i) + 0.5*x*(dc(i) + c6(i)*(1. - TWO3RDS*x)));
-    %if (x.ge.1) write(*,*)'Courant number is bigger than 1',x ! jgj 10/6/06
     x = max(0., vel(i)*(dt/dx));
-    %if (x.ge.1) write(*,*)'Courant number is bigger than 1',x ! jgj 10/6/06
     fp(i) = x*(cr(i) - 0.5*x*(dc(i) - c6(i)*(1. - TWO3RDS*x)));
 end
 
 %Compute fluxes from boundary cells assuming uniform distribution
 
 if vel(1) > 0
-    x = vel(1)*(dt/dx)
-    fp(1) = x*con(1)
+    x = vel(1)*(dt/dx);
+    fp(1) = x*con(1);
 end
 
 if vel(nn-1) < 0
@@ -90,11 +91,14 @@ end
 
 %Update concentrations
 
-flxarr(1) = (fp(1) - fm(2))*(dx/dt)
+flxarr(1) = (fp(1) - fm(2))*(dx/dt);
+
 for i = 2 : nn-1
-    flxarr(i) = (fp(i) - fm(i+1))*(dx/dt)
-    con(i) = con(i) - mscl(i)*(flxarr(i) - flxarr(i-1))*(dt/dx)
+    flxarr(i) = (fp(i) - fm(i+1))*(dx/dt);
+    con(i) = con(i) - mscl(i)*(flxarr(i) - flxarr(i-1))*(dt/dx);
 end
-flux1 = mscl(2)*flxarr(1)
-flux2 = mscl(nn-1)*flxarr(nn-1)
+flux1 = mscl(2)*flxarr(1);
+flux2 = mscl(nn-1)*flxarr(nn-1);
+
+
 end
