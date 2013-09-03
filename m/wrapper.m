747f856305af972b9f1766f6dde0873b4ad76840
Sebastian Arnoldt <sebastian.arnoldt@gmail.com>
2013-09-03 13:12:26 +0200

    matlab version complete

---
+++ m/wrapper.m
@@ -0,0 +1,12 @@
+nn = 90
+dt = 900
+dx = 36000
+conInit = zeros(nn,1) + 1
+velInit = zeros(nn,1) + 0.5
+mscl = zeros(nn,1) + 1
+flxarr = zeros(nn,1)
+
+flux1 = 0
+flux2 = 0
+
+hadvppm(nn, dt, dx, conInit, velInit, mscl, flxarr, flux1, flux2)
